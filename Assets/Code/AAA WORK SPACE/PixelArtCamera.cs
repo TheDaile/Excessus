7ae25bbe7e0ca3a313aafb10af28394e80e4b031
Krzysztof <marczynskikrzysztof7@gmail.com>
2026-07-02 14:48:56 +0200

    fix

--- Assets/Code/AAA WORK SPACE/PixelArtCamera.cs
+++ Assets/Code/AAA WORK SPACE/PixelArtCamera.cs
@@ -1,13 +1,12 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PixelArtCamera : MonoBehaviour
 {
-    [SerializeField] private Camera _camera; 
-    [SerializeField] private RawImage _rawImage; 
+    [SerializeField] private Camera _camera;
+    [SerializeField] private RawImage _rawImage;
 
-    [SerializeField] private int _cameraHeight; 
+    [SerializeField] private int _cameraHeight = 400;
 
     private RenderTexture _renderTexture;
 
@@ -16,14 +15,18 @@ public class PixelArtCamera : MonoBehaviour
         UpdateRenderTexture();
     }
 
-        public void UpdateRenderTexture()
+    public void UpdateRenderTexture()
     {
-        if(_renderTexture != null)
+        if (_camera == null || _rawImage == null || _cameraHeight <= 0)
         {
-            _renderTexture.Release();
+            return;
         }
 
-        float aspectRatio = (float)Screen.width / Screen.height;
+        DisposeRenderTexture();
+
+        _rawImage.rectTransform.SetAsFirstSibling();
+
+        float aspectRatio = (float)Screen.width / Mathf.Max(1, Screen.height);
         int cameraWidth = Mathf.RoundToInt(aspectRatio * _cameraHeight);
 
         _renderTexture = new RenderTexture(cameraWidth, _cameraHeight, 16, RenderTextureFormat.ARGB32);
@@ -32,5 +35,27 @@ public class PixelArtCamera : MonoBehaviour
         _renderTexture.Create();
         _camera.targetTexture = _renderTexture;
         _rawImage.texture = _renderTexture;
+    }
+
+    private void OnDestroy()
+    {
+        DisposeRenderTexture();
+    }
+
+    private void DisposeRenderTexture()
+    {
+        if (_renderTexture == null)
+        {
+            return;
+        }
+
+        if (_camera != null && _camera.targetTexture == _renderTexture)
+        {
+            _camera.targetTexture = null;
+        }
+
+        _renderTexture.Release();
+        Destroy(_renderTexture);
+        _renderTexture = null;
     }
 }
